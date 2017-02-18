b95dff10e45e9c5b8e6d54fd090743757bef4b81
husky <shuaike945@gmail.com>
2017-02-18 21:06:18 +0800

    backup-20170218

---
+++ habitica-20161001.1122/habitica-autoloads.el
@@ -0,0 +1,21 @@
+;;; habitica-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "habitica" "habitica.el" (22516 24714 0 0))
+;;; Generated autoloads from habitica.el
+
+(autoload 'habitica-tasks "habitica" "\
+Main function to summon the habitica buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; habitica-autoloads.el ends here
---
+++ org-attach-screenshot-20160928.258/org-attach-screenshot-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-attach-screenshot" "20160928.258" "screenshots integrated with org attachment dirs" 'nil :url "https://github.com/dfeich/org-screenshot" :keywords '("org"))
